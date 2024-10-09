02aaa04aa10d18985402b1dc88b73281e614314b
Astro <astrowolf.stream@gmail.com>
2024-10-09 14:00:02 +0200

    náder

---
+++ orai01/orai01/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace orai01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string szoveg = args[0];
+            int szam = Convert.ToInt32(args[1]);
+
+
+            for (int i = 0; i < szam; i++)
+            {
+                Console.Write(szoveg);
+                Console.Write(' ');
+            }
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ szamologep/szamologep/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace szamologep
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+
+                double x = Convert.ToDouble(args[0]);
+                string operacio = args[1];
+                double y = Convert.ToDouble(args[2]);
+
+                double eredmeny = 0;
+                if (operacio == "+") { eredmeny = x + y; }
+                if (operacio == "-") { eredmeny = x - y; }
+                if (operacio == "*") { eredmeny = x * y; }
+                if (operacio == "/") { eredmeny = x / y; }
+                
+
+                if(operacio == "sin") {eredmeny = x Math.Sin
+                Console.WriteLine("Az eredmény: " + eredmeny);
+
+            }
+            else
+            {
+                Console.WriteLine("nics paraméter");
+                Console.WriteLine("büntetésből szét fogjuk húzni a lukadat");
+            }
+
+
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
