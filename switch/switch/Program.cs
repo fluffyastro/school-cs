b23bdc1106c5f8a77eee58f1b74db6a0984adf6c
Astro <astrowolf.stream@gmail.com>
2024-10-16 12:55:32 +0200

    meow (10/16/2024)

---
+++ switch/switch/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Switch01;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] jegyek = { 5, 5, 3, 5, 1, 5, 4, 5 };
+
+        int szum = 0;
+        for (int i = 0; i < jegyek.Length; i++)
+        {
+            szum += jegyek[i];
+        }
+
+        double atlag = Convert.ToDouble(szum) / jegyek.Length;
+
+
+        switch (atlag)
+        {
+            case > 4.5:
+                Console.WriteLine("Jeles! :3");
+                break;
+            case > 3.5:
+                Console.WriteLine("Jó! :D");
+                break;
+            case > 2.5:
+                Console.WriteLine("Közepes! :/");
+                break;
+            case > 1.5:
+                Console.WriteLine("Elégséges! :(");
+                break;
+            default:
+                Console.WriteLine("Elégtelen! :-(");
+                break;
+        }
+    }
+}
