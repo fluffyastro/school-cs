b7bcfb5bc2207ce38830c1643acc2b5fc00dc1f8
Astro <astrowolf.stream@gmail.com>
2024-10-16 12:25:27 +0200

    doga2

---
+++ doga2/doga2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace doga2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // tömbök elnevezése, tömb tartalmainak megadása
+
+            string[] honap_tomb = ["Január", "Február", "Március", "Április", "Május", "Június", "Július", "Augusztus", "Szeptember", "Október", "November", "December"];
+            string[] nap_tomb = ["Vasárnap", "Hétfő", "Kedd", "Szerda", "Csütörtök", "Péntek", "Szombat"];
+
+
+            // szám(ok) bekérése/beírása
+
+            Console.WriteLine("Írjon be egy hónap számát!");
+            int honap_bekeres = Convert.ToInt32(args[0]);
+            int nap_bekeres = Convert.ToInt32(args[1]);
+            {
+                {
+                    Console.WriteLine(honap_tomb[honap_bekeres-1]);
+                    Console.WriteLine(nap_tomb[nap_bekeres - 1]);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
