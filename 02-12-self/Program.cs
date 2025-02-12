5221a6550d64b675bf17536577b9e2153c597789
Astro <astrowolf.stream@gmail.com>
2025-02-12 13:35:38 +0100

    :D
    D: <-- literally me after this

---
+++ 02-12-self/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _02_12_self
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            string output = "";
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    for (int chr = 0; chr < 50; chr++)
+                    {
+                        output += Convert.ToChar(rnd.Next(97, 122));
+                    }
+                    output += Environment.NewLine;
+                }
+                File.WriteAllText($"szoveg0{i}.txt", output);
+            }
+        }
+    }
+}
